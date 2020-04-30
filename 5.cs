535eca4d7bc35927c2d6d9023beb2c20b265d538
yhrtyrst <62638430+yhrtyrst@users.noreply.github.com>
2020-04-30 18:47:49 +0800

    Add files via upload

---
+++ 5.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class Test
+{
+	public static void Main(string[] args)
+	{	
+		int x; //min
+		int y;
+		int z; //max
+		
+		/* static void Swap(int a, int b)
+		{
+			int temp = a;
+			a=b;
+			b=temp;
+		}*/
+		
+		List<int> myLists = new List<int>();
+		myLists.Add(x);
+		myLists.Add(y);
+		myLists.Add(z);
+		while(x)
+		
+			
+			
+		Console.WriteLine(Convert.Toint32(Console.ReadLine()));
+		
+	}
+}
